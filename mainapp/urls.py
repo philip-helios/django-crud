3979e3177dfc12d31e9fef028d29240c56c140ec
philip-helios <philip.kavan@heliosinsider.com>
2023-02-06 13:39:54 +0600

    added function to delete users

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from mainapp import views
 
 urlpatterns = [
-    path('',views.add_show, name="addandshow")
+    path('',views.add_show, name="addandshow"),
+    path('delete/<int:id>/',views.delete_data, name="deletedata")
 ]
