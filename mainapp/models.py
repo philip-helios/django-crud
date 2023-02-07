ae6f4d04407be0031737cb1a888d313d4471cf8a
philip-helios <philip.kavan@heliosinsider.com>
2023-02-07 10:23:47 +0600

    added regex validation field for contact field input and customized the error message

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
 # Create your models here.
+
+def form_validator(value):
+    if value[0] != 's':
+        raise models.ValidationError('Name should start with s')
 
 class User(models.Model):
     name=models.CharField(max_length=70)
-    contact=models.CharField(max_length=100)
+    contact=models.CharField(max_length=100,validators=[RegexValidator(r'^(?:\+88|88)?(01[3-9]\d{8})$', message="Phone number is not valid")])
 
 
